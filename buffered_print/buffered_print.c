b657c94eed7c8b926a849e40154e660cc82f2998
rosenrose <crazytempler@gmail.com>
2024-08-19 14:35:16 +0900

    buffered_print

---
+++ buffered_print/buffered_print.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include "buffered_print.h"
+
+#define BUFFER_LEN (32)
+
+static size_t s_buffer_idx = 0u;
+static char s_buffer[BUFFER_LEN];
+
+void buffered_print(const char *src)
+{
+    size_t num_left = strlen(src);
+
+    while (num_left > 0)
+    {
+        size_t copy_cnt = BUFFER_LEN - 1 - s_buffer_idx;
+        const int is_buffer_empty = s_buffer_idx == 0;
+
+        if (copy_cnt > num_left)
+        {
+            copy_cnt = num_left;
+        }
+
+        s_buffer_idx += copy_cnt;
+        num_left -= copy_cnt;
+
+        if (is_buffer_empty)
+        {
+            strncpy_s(s_buffer, BUFFER_LEN, src, copy_cnt);
+            s_buffer[s_buffer_idx] = '\0';
+        }
+        else
+        {
+            strncat_s(s_buffer, BUFFER_LEN, src, copy_cnt);
+        }
+
+        src += copy_cnt;
+
+        if (s_buffer_idx == BUFFER_LEN - 1)
+        {
+            printf("%s\n", s_buffer);
+            s_buffer_idx = 0;
+        }
+    }
+}
---
+++ buffered_print/main.c
@@ -0,0 +1,19 @@
+#include "buffered_print.h"
+
+int main(void)
+{
+    buffered_print("Hello, ");
+    buffered_print("World. ");
+    buffered_print("C is awesome! ");
+    buffered_print("C# is awesome too! ");
+    buffered_print("Is Java better? ");
+
+    buffered_print("Hello, ");
+    buffered_print("Hello, ");
+    buffered_print("Hello, ");
+    buffered_print("Hello, ");
+    buffered_print("Hello, ");
+    buffered_print("Hello, ");
+    buffered_print("Hello, ");
+    return 0;
+}
