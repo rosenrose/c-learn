3285812e995d8571ae940ea5dfb89e402eb6df5f
rosenrose <crazytempler@gmail.com>
2024-08-22 15:42:52 +0900

    arguments

--- 10_file_utils/main.c
+++ 10_file_utils/main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 #include "file_utils.h"
 
-int main(const int argc, const char *const args[])
+void print_help(void)
+{
+    puts("usage: a <repeat>");
+    puts("usage: a <src> <dst>");
+}
+
+int main(const int argc, const char *const argv[])
 {
     switch (argc)
     {
     case 2:
-        print_with_repeats("repeat.txt");
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+        {
+            print_help();
+        }
+        else
+        {
+            print_with_repeats(argv[1]);
+        }
         break;
     case 3:
-        copy_file(args[1], args[2]);
+        copy_file(argv[1], argv[2]);
         break;
 
     default:
-        return -1;
+        print_help();
+        perror("Invalid arguments");
+        return 1;
     }
 
     return 0;
